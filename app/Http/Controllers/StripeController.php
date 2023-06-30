11dd54a4268644a1d853c3f0b38b78d1400e9785
Gavin Kimpson <gkimpson@gmail.com>
2023-06-30 08:32:14 +0100

    basic mvc

---
+++ app/Http/Controllers/StripeController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StripeController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function checkout()
+    {
+        \Stripe\Stripe::setApiKey(config('stripe.sk'));
+
+        $session = \Stripe\Checkout\Session::create([
+            'line_items' => [
+                [
+                    'price_data' => [
+                        'currency' => 'gbp',
+                        'product_data' => ['name' => 'T-shirt'],
+                        'unit_amount' => 500,
+                    ],
+                    'quantity' => 2,
+                ],
+            ],
+            'mode' => 'payment',
+            'success_url' => route('success'),
+            'cancel_url' => route('cancel'),
+        ]);
+
+        return redirect()->away($session->url);
+    }
+
+    public function success()
+    {
+        return view('success');
+    }
+
+    public function cancel()
+    {
+        return view('cancel');
+    }
+}
